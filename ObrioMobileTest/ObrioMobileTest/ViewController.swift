e9c5d3781f147fa03cdeed5469aa4349ea96304c
Sergii Mikushev <sergemi.kh@gmail.com>
2020-11-06 18:54:36 +0200

    init project and design main screen

---
+++ ObrioMobileTest/ObrioMobileTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObrioMobileTest
+//
+//  Created by sergemi on 06.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
